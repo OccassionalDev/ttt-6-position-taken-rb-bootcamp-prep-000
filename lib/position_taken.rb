ac18667c964cfae83fc35b84779dea040581b3e3
active-class-0481 <bestlittleangel1@gmail.com>
2019-12-16 03:51:58 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,12 @@
 # code your #position_taken? method here!
 
 def position_taken(array, index)
-  if ()
+  if (array[index] == " " || array[index] == "")
+    return FALSE
   
+  else
+    return true
+  
+  end
   
 end
